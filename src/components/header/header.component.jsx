3b22cdabbbfa58dbbe69f7dc42b6bc1c1ec8f103
Sai Aung Naing Tun <saisaiant@gmail.com>
2020-05-03 23:47:14 +0630

    add sign in & sign up component & add firebase database

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -24,7 +24,7 @@ const Header = ({ currentUser }) => (
           SIGN OUT
         </div>
       ) : (
-        <Link className="option" to="/sigin">
+        <Link className="option" to="/signin">
           SIGN IN
         </Link>
       )}
